c87707a283fb4994ef999de572b47c2161c90ffb
Waqas Ahmad <ekaur45@gmail.com>
2024-01-10 17:37:16 +0500

    first

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const config = require("./utils/config");
+const apiRouter = require("./routes");
+const app = express();
+const server = require("http").createServer(app);
+const responser = require("./utils/res.util");
+app.use(responser);
+app.use(express.static("public"));
+
+
+app.use("/api",apiRouter);
+
+app.use("*",(req,res,next)=>{
+    res.NotFound(req.originalUrl,"Resource not found.");
+})
+
+const port = config.PORT || 3000;
+server.listen(port,()=>{
+    console.log(`http://localhost:${port}/api-doc`);
+})
