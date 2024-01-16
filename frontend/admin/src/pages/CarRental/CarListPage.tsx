33f878729d234ef87f8785ab7adf931637a91153
Waqas Ahmad <ekaur45@gmail.com>
2024-01-16 18:49:48 +0500

    use fetch added

--- frontend/admin/src/pages/CarRental/CarListPage.tsx
+++ frontend/admin/src/pages/CarRental/CarListPage.tsx
@@ -1,47 +1,27 @@
+import { useFetch } from "../../hooks/useFetch"
+const URL = 'http://localhost:8000/api/';
 export default function CarListPage() {
+    const { data, error } = useFetch<any>(URL+'', { method: "GET" });
+    if (error) return <>Error</>
+    if (!data) return <>Loading...</>
     return (<>
         <div className="grid grid-cols-1 gap-5 sm:grid-cols-4">
+            {
+                data && data.map((e: any, i: number) => {
+                    return <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark pb-0">
+                        <div className="grid grid-cols-1 gap-9 sm:grid-cols-2 p-6.5">
+                            Car Details and actions here
+                        </div>
+                    </div>
+                })
+            }
+
             <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark pb-0">
                 <div className="grid grid-cols-1 gap-9 sm:grid-cols-2 p-6.5">
                     Car Details and actions here
                 </div>
             </div>
-            <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark pb-0">
-                <div className="grid grid-cols-1 gap-9 sm:grid-cols-2 p-6.5">
-                Car Details and actions here
-                </div>
-            </div>
-            <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-                <div className="grid grid-cols-1 gap-9 sm:grid-cols-2 p-6.5">
-                Car Details and actions here
-                </div>
-            </div>
-            <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-                <div className="grid grid-cols-1 gap-9 sm:grid-cols-2 p-6.5 pb-0">
-                Car Details and actions here
-                </div>
-            </div>
-            <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-                <div className="grid grid-cols-1 gap-9 sm:grid-cols-2 p-6.5 pb-0">
-                Car Details and actions here
-                </div>
-            </div>
-            <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-                <div className="grid grid-cols-1 gap-9 sm:grid-cols-2 p-6.5 pb-0">
-                Car Details and actions here
-                </div>
-            </div>
-            <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark pb-0">
-                <div className="grid grid-cols-1 gap-9 sm:grid-cols-2 p-6.5">
-                Car Details and actions here
-                </div>
-            </div>
-            <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark pb-0">
-                <div className="grid grid-cols-1 gap-9 sm:grid-cols-2 p-6.5">
-                Car Details and actions here
-                </div>
-            </div>
         </div>
-        </>
+    </>
     )
 }
