8c3db291b838b395edec76f7fd1c8de4af682cc7
Waqas Ahmad <ekaur45@gmail.com>
2024-01-31 14:19:24 +0500

    front end

--- frontend/landing/src/components/DefaultLayout.tsx
+++ frontend/landing/src/components/DefaultLayout.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
-
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 export default function DefaultLayout() {
     return (<>
+    
+       <ToastContainer />
         <div className="preloader">
             <div className="loader">
                 <div className="ytp-spinner">
---
+++ frontend/landing/src/pages/bookings/BookingListPage.tsx
@@ -0,0 +1,7 @@
+import { Fragment } from "react";
+
+export default function BookingListPage(){
+    return(<Fragment>
+        Booking List Page
+    </Fragment>)
+}
