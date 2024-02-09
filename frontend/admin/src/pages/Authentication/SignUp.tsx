85afaf77c1d5e7069a0f77d324e97bf8a8a4fdc9
Waqas Ahmad <ekaur45@gmail.com>
2024-02-09 23:38:21 +0500

    oper

--- frontend/admin/src/pages/Authentication/SignUp.tsx
+++ frontend/admin/src/pages/Authentication/SignUp.tsx
@@ -195,7 +195,7 @@ const SignUp = () => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+            <Listbox.Options className="z-10 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
               {userTypes.map((person, personIdx) => (
                 <Listbox.Option
                   key={personIdx}
