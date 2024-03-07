c54b7f2915038cbed1164f3d173f3d6fa5c979f9
Waqas Ahmad <ekaur45@gmail.com>
2024-03-07 16:09:20 +0500

    .

--- frontend/admin/src/pages/Dashboard/ECommerce.tsx
+++ frontend/admin/src/pages/Dashboard/ECommerce.tsx
@@ -1,14 +1,59 @@
+import { useState } from "react";
+import StorageUtil from "../../utils/storage-util";
+import { UserType } from "../../components/DropdownUser";
 
 
 const ECommerce = () => {
-  return (
-    <>
+  const [user] = useState(StorageUtil.getUser());
+  if(user.userType == UserType["Car Rental"]){
+    return (
+      <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-     
+        Rental
       </div>
 
-      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-      
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">      
+        <div className="col-span-12 xl:col-span-8">
+        </div>
+      </div>
+      </>
+    )
+  }
+  if(user.userType == UserType["Event Decorator"]){
+    return (
+      <>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">     
+      Decorator
+      </div>
+
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">      
+        <div className="col-span-12 xl:col-span-8">
+        </div>
+      </div>
+      </>
+    )
+  }
+  if(user.userType == UserType["Venue Provider"]){
+    return (
+      <>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">     
+      Venue
+      </div>
+
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">      
+        <div className="col-span-12 xl:col-span-8">
+        </div>
+      </div>
+      </>
+    )
+  }
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">     
+      Admin
+      </div>
+
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">      
         <div className="col-span-12 xl:col-span-8">
         </div>
       </div>
