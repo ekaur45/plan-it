a576800b899f72dfe5365de7ca1a27aac6193242
Waqas Ahmad <ekaur45@gmail.com>
2024-05-17 11:46:50 +0500

    port

--- frontend/landing/src/utils/config.util.ts
+++ frontend/landing/src/utils/config.util.ts
@@ -1,6 +1,6 @@
 const CONFIG = {
-    BaseUrl:"http://localhost:8081/",
-    ApiUrl:"http://localhost:8081/api/",
+    BaseUrl:"http://localhost:8000/",
+    ApiUrl:"http://localhost:8000/api/",
     AdminUrl:"http://localhost:5173/"
 }
 
